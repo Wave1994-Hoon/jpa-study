18d08d0286645982939fba959d3b3b804527b8e8
kwanghoon.kim <kk03187693@gmail.com>
2020-12-06 00:39:32 +0900

    add 기본키매핑

--- src/main/java/helloJPA/Member.java
+++ src/main/java/helloJPA/Member.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 @Entity
 public class Member {
-    @Id
+    // Identity 전략은 디비에 직접 PK를 넣으면 안, DB에서 Insert를 해야함 -> null로 들어오면 DB에서 값을 세팅함 -> DB에 값이 들어가야 PK 값을 알 수 있
+    // 따라서 영속성 컨택스트에서는 ID 값을 1차 캐시에서 사용해야하기 때문에 예외적으로 persist 시점에 데이터베이스에 Insert 쿼리를 날린다.
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
