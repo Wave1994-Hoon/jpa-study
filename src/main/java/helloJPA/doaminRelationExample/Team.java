584385998322721ef6ec61b7e70eadc941d1aa64
kwanghoon.kim <kk03187693@gmail.com>
2020-12-07 00:48:17 +0900

    add 양방향 연관관계

--- src/main/java/helloJPA/doaminRelationExample/Team.java
+++ src/main/java/helloJPA/doaminRelationExample/Team.java
@@ -1,9 +1,8 @@
 package helloJPA.doaminRelationExample;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,4 +13,30 @@ public class Team {
     @Column(name = "USERNAME")
     private String userName;
 
+    @OneToMany(mappedBy = "team") // Member 클래스에 있는 team과 매핑이 되어 있다.
+    private List<Member> members = new ArrayList<>(); // add 할 때, NPE 방지
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 }
